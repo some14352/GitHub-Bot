f8cc433da26213ea74e5aef4e2911528f5e82f45
neelbavarva <neelbavarva5@gmail.com>
2021-04-20 01:43:41 -0400

    header image

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+
+for i in range(366):
+    d = str(i) + ' days ago '
+    with open('bot.txt', 'a') as file:
+        file.write(d+'\n')
+    os.system('git add bot.txt')
+    os.system('git commit --date="' + d + '" -m "hack commit"' )
+
+os.system('git push -u origin main')
